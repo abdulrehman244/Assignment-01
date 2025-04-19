f733831ecd149f766d8a04b6bbfc91abdbb3fcb5
abdulrehman244 <abdulrehman.ali6288@gmail.com>
2025-04-20 00:12:10 +0500

    Add files via upload

---
+++ assign/index.dart
@@ -0,0 +1,13 @@
+void main(){
+  string name = "Abdul Rehman Ali";
+  int age = 18;
+  string className = "9th class";
+  double percentage = 70.3;
+  string previousResult = "pass";
+
+  print(name);
+  print(age);
+  print(className);
+  print(percentage);
+  print(previousResult);
+};
